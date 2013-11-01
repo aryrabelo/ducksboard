287b4503d771c981fa764a0a505597bd527b9da7
Ary Ribeiro Rabelo <aryrabelo@gmail.com>
2013-11-01 19:14:44 -0300

    ajeitando os testes

--- test/lib/ducksboard/completion_test.rb
+++ test/lib/ducksboard/completion_test.rb
@@ -5,16 +5,14 @@ describe Ducksboard::Completion do
   describe '#completion=' do
     let(:completion) { Ducksboard::Completion.new(1) }
 
-    before { completion.current = 200 }
+    before { 
+    	completion.current = 200
+    	completion.min = 0
+    	completion.max = 5000 
+     }
 
     it { completion.data[:value][:current].must_equal(200) }
-
-    before { completion.min = 0 }
-
     it { completion.data[:value][:min].must_equal(0) }
-
-      before { completion.max = 5000 }
-
     it { completion.data[:value][:max].must_equal(5000) }
   end
 end
